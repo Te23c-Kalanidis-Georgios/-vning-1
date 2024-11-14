e6bf33448f1e2b2a3cf8f49580412d82841b9767
Georgios Kalanidis <georgios.kalanidis@elev.ga.ntig.se>
2024-11-14 12:48:42 +0100

    första commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,152 @@
+﻿//if (6 >= 3)
+//{
+    //Console.WriteLine("Hello, World!");
+//}
+
+
+
+//Console.WriteLine("Enter your username:");
+//string username = Console.ReadLine();
+
+//if (username == "kalleanka")
+//{
+    //Console.WriteLine("Welcome!");
+//}
+
+
+
+//Console.WriteLine("Enter your username:");
+//string username = Console.ReadLine();
+
+//Console.WriteLine("Enter your password:");
+//string password = Console.ReadLine();
+
+//if (username == "kalleanka" && password == "12345")
+//{
+    //Console.WriteLine("Welcome!");
+//}
+//else
+//{
+   // Console.WriteLine("Wrong username or password");
+//}
+
+
+
+
+//int count = 0;
+//while (count < 32)
+//{
+    //Console.WriteLine("Hello, World");
+    //count++;
+//}
+
+//string correctPassword = "12345";
+//string inputPassword = "";
+
+//while (inputPassword != correctPassword)
+//{
+    //Console.WriteLine("Enter your password:");
+    //inputPassword = Console.ReadLine();
+
+   // if (inputPassword != correctPassword)
+    //{
+       // Console.WriteLine("Incorrect password, try again.");
+   // }
+//}
+
+//Console.WriteLine("Access granted.");
+
+
+
+
+
+
+
+//for (int i = 0; i < 5; i++)
+//{
+   // Console.WriteLine("Enter a number:");
+    //string input = Console.ReadLine();
+   // int number = int.Parse(input);  // Konverterar input från string till int
+
+    //if (number > 5)
+    //{
+       // Console.WriteLine("högre än 5!");
+    //}
+//}
+
+
+
+
+
+//string input;
+//int number;
+
+//while (true)
+//{
+    //Console.WriteLine("Enter a number:");
+    //input = Console.ReadLine();
+
+ 
+    //if (int.TryParse(input, out number))
+    //{
+        //Console.WriteLine("Valid number entered: " + number);
+        //break;  // Avslutar loopen om konverteringen lyckas
+    //}
+    //else
+    //{
+       // Console.WriteLine("Invalid input, please enter a valid number.");
+    //}
+//}
+
+
+
+
+
+
+
+
+//using System;
+
+//class Program
+//{
+   // static void Main()
+   // {
+      //  Random random = new Random();
+       // int targetNumber = random.Next(1, 101);  // Slumpar ett tal mellan 1 och 100
+       // int userGuess = -1;
+
+        //Console.WriteLine("Välkommen till gissa siffran-spelet!");
+        //Console.WriteLine("Gissa ett tal mellan 1 och 100.");
+
+        //while (userGuess != targetNumber)
+        //{
+           // Console.WriteLine("Skriv in din gissning:");
+
+          
+            //string input = Console.ReadLine();
+            //if (int.TryParse(input, out userGuess))
+            //{
+                //if (userGuess < targetNumber)
+               // {
+                   // Console.WriteLine("För lågt, försök igen!");
+               // }
+                //else if (userGuess > targetNumber)
+               // {
+                   // Console.WriteLine("För högt, försök igen!");
+               // }
+               // else
+               // {
+                  //  Console.WriteLine("Grattis! Du gissade rätt!");
+              //  }
+           // }
+           // else
+           // {
+               // Console.WriteLine("Ogiltig inmatning, vänligen ange ett heltal.");
+           // }
+        //}
+   // }
+//}
+
+
+
+
